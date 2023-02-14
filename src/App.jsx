9280284bd08537c5914aa4fcd06fe90563507417
ArturDemich <strelokar@gmail.com>
2023-02-14 22:40:08 +0200

    start project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Hello World !!!</h1>
+
+    </div>
+  )
+}
+
+export default App
